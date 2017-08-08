0a2e3f1ce963d0ccf04156fca1a98b99ea698627
Mairaleja <30596008+Mairaleja@users.noreply.github.com>
2017-08-08 08:28:19 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Saludo</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="css/bootstrap.min.css">
+      <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <div class="container ">
+      <hr>
+      <h1><?php echo  "Algoritmos en php " ?></h1>
+      <hr>
+      <h2>Ejercicios</h2>
+      <ol>
+        <div class="row">
+        <div class="col-6">
+
+          <li class=""><a href="a/0_saludo.php"><button class="btn btn-primary" type="button" name="button ">Saludo</button></li></a>
+          <li class=""><a href="a/1_PagoNeto.php"><button class="btn btn-primary" type="button" name="button ">Salario neto</button></li></a>
+          <li class=""><a href="a/2_operaciones.php"><button class="btn btn-primary" type="button" name="button">Operaciones de 2 enteros</button></a></li>
+          <li class=""><a href="a/3_doble.php"><button class="btn btn-primary" type="button" name="button">Doble y tripe de un entero</button></a></li>
+          <li class=""><a href="a/4_promedio.php"><button class="btn btn-primary" type="button" name="button">Promedio de 3 notas</button></a></li>
+          <li class=""><a href="a/5_cuadrado.php"><button class="btn btn-primary" type="button" name="button">Cuadrado de un numero</button></a></li>
+          <li class=""><a href="a/6_triangulo.php"><button class="btn btn-primary" type="button" name="button">Area de un triangulo</button></a></li>
+          <li class=""><a href="a/7_rectangulo.php"><button class="btn btn-primary" type="button" name="button">Area de un rectangulo</button></a></li>
+          <li class=""><a href="a/8_circulo.php"><button class="btn btn-primary" type="button" name="button">Area de un circulo</button></a></li>
+          <li class=""><a href="a/9_paralelo.php"><button class="btn btn-primary" type="button" name="button">area de un paralelograma</button></a></li>
+          <li class=""><a href="a/10_rombo.php"><button class="btn btn-primary" type="button" name="button">Area y pelimetro de un rombo</button></a></li>
+        </div>
+        <div class="col-6">
+
+          <li class=""><a href="a/11_deltoide.php"><button class="btn btn-primary" type="button" name="button">Area y perimetro de un deltoide</button></a></li>
+          <li class=""><a href="a/12_millas.php"><button class="btn btn-primary" type="button" name="button">Conversión millas a kilometros</button></a></li>
+          <li class=""><a href="a/13_kelvin.php"><button class="btn btn-primary" type="button" name="button">Conversión de grados kelvin a grados farenheit</button></a></li>
+          <li class=""><a href="a/15_operacion.php"><button class="btn btn-primary"type="button" name="button">Operaciones de dos enteros 2</button></a></li>
+          <li class=""><a href="a/16_elevado.php"><button class="btn btn-primary"type="button" name="button">Numero entero elevado al cubo</button></a></li>
+          <li class=""><a href="a/17_horas.php"><button class="btn btn-primary"type="button" name="button">Conversion de tiempo</button></a></li>
+          <li class=""><a href="a/18_kilos.php"><button class="btn btn-primary"type="button" name="button">Conversion Kilogramos a gramos</button></a></li>
+          <li class=""><a href="a/19_raizsqrt.php"><button class="btn btn-primary"type="button" name="button">Raiz cuadrada de un numero con sqrt</button></a></li>
+          <li class=""><a href="a/20_pitagoras.php"><button class="btn btn-primary"type="button" name="button">Teorema de pitagoras</button></a></li>
+        </div>
+        </div>
+      </ol>
+      <script src="js/jquery.js"></script>
+      <script src="js/bootstrap.min.js"></script>
+    </div>
+  </body>
+</html>
